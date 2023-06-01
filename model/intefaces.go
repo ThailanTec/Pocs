cc1422fe09b4c979a35c9e966c7e4e2c0a442946
Thailan dos Santos de Jesus <thailan.jesus@engdb.com.br>
2023-06-01 18:01:02 -0300

    http alter

--- model/intefaces.go
+++ model/intefaces.go
@@ -12,8 +12,3 @@ type CreateDB struct {
 	SourceName              string                 `json:"sourceName"`
 	Format                  string                 `json:"format"`
 }
-
-const (
-	Url           = "http://34.105.89.111/api/v1/sources/create"
-	Authorization = "Basic YWlyYnl0ZV9hZG1pbjo3aVVld3p1WiFTWExs"
-)
--- strategy/http.go
+++ strategy/http.go
@@ -11,20 +11,26 @@ import (
 )
 
 func NewHttpRequest(inputB model.CreateDB) error {
+
+	const (
+		Url           = "http://34.105.89.111/api/v1/sources/create"
+		Authorization = "Basic YWlyYnl0ZV9hZG1pbjo3aVVld3p1WiFTWExs"
+	)
+
 	mash := inputB
 	dbMarshal, err := json.Marshal(mash)
 	if err != nil {
 		return err
 	}
 
-	url := model.Url
+	url := Url
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(dbMarshal))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
 	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", model.Authorization)
+	req.Header.Add("Authorization", Authorization)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
